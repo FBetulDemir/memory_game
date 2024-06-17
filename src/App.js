8b9c33bfdd2ac62ce5c1d2bcb852ad3c1c66a59b
FBetulDemir <fatmabetuld@gmail.com>
2024-06-17 13:02:21 +0200

    Fixed the grid layout for the cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/images/helmet-1.png" },
+  { "src": "/images/potion-1.png" },
+  { "src": "/images/ring-1.png" },
+  { "src": "/images/scroll-1.png" },
+  { "src": "/images/shield-1.png" },
+  { "src": "/images/sword-1.png" },
+] 
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id:Math.random()}))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front-card" src={card.src} alt="card front side" />
+              <img className="back-card" src="images/cover.png" alt="card back side" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
